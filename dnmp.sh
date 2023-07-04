6e24b7fddb16e8e8c3f58010262bdc24fdcac2e5
RyanY610 <106591422+RyanY610@users.noreply.github.com>
2023-07-04 21:04:19 +0800

    Update dnmp.sh

--- dnmp.sh
+++ dnmp.sh
@@ -35,4 +35,3 @@ cd /var/dnmp/ && docker-compose restart
 cd /var/dnmp/ && cp php7.4_cron.service /etc/systemd/system/
 cd /var/dnmp/ && cp php8.1_cron.service /etc/systemd/system/
 
-rm -rf /var/dnmp/dnmp.sh
